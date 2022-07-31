a783386456cf30a04c8dc1a850eef74bb27c2a88
MKM327 <85278762+MKM327@users.noreply.github.com>
2022-07-31 13:04:27 +0300

    Added details tab

--- TodoApp/script.js
+++ TodoApp/script.js
@@ -10,3 +10,18 @@ const closeAddMenu = () => {
   menu.classList.remove("open");
   menu.classList.add("closed");
 };
+function rotate(rotateId, detailId) {
+  let btnClassList = document.getElementById(rotateId).classList;
+  let todoDetails = document.getElementById(detailId).classList;
+  if (btnClassList.contains("down")) {
+    btnClassList.remove("down");
+    btnClassList.add("right");
+    todoDetails.remove("details-open");
+    todoDetails.add("details-closed");
+  } else {
+    btnClassList.remove("right");
+    btnClassList.add("down");
+    todoDetails.remove("details-closed");
+    todoDetails.add("details-open");
+  }
+}
