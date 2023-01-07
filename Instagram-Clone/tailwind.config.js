87b3cf1b5cc622614eb92b7d9518bb4a4efe114a
MKM327 <85278762+MKM327@users.noreply.github.com>
2023-01-07 13:23:32 +0300

    t

--- Instagram-Clone/tailwind.config.js
+++ Instagram-Clone/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./srcs/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        96: "24rem",
+        128: "32rem",
+      },
+      width: {
+        96: "24rem",
+        128: "32rem",
+        "1/1": "100%",
+      },
+    },
   },
   plugins: [],
 };
