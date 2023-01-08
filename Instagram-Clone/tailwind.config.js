620108f12aacdc7a1f75656f63dac08f523a0bdb
MKM327 <85278762+MKM327@users.noreply.github.com>
2023-01-08 22:00:36 +0300

    added main

--- Instagram-Clone/tailwind.config.js
+++ Instagram-Clone/tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
         128: "32rem",
         "1/1": "100%",
       },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+    screens: {
+      //add max width media queries
+      "m-sm": { max: "640px" },
+      "m-md": { max: "768px" },
+      "m-lg": { max: "1024px" },
+      "m-xl": { max: "1280px" },
+      "m-2xl": { max: "1536px" },
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
   },
   plugins: [],
